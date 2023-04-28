f782a3fc5fc88a635b1b83c842a555962c6240b5
Vincenzo Picone <118441474+Vincenzopicone@users.noreply.github.com>
2023-04-28 22:24:34 +0200

    Initial commit

---
+++ src/test/java/it/vincenzopicone/gestioneincendi/GestioneIncendiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vincenzopicone.gestioneincendi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneIncendiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
